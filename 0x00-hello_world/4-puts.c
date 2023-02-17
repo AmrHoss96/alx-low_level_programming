e2bcfbb6f6e5fa1cab2150620770b770120e3479
Amr <amrhoss96@gmail.com>
2023-02-16 16:29:50 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-int main (void)
+int main(void)
 {
-char Mystr[] = "Programming is like a multilingual puzzle";
+char Mystr[] = "\"Programming is like a multilingual puzzle";
+
 puts(Mystr);
+
 return (0);
 }
