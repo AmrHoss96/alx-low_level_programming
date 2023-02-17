3d436b60755c03c740492f58342cc4b18f8af36b
Amr <amrhoss96@gmail.com>
2023-02-16 16:37:21 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description :' This is a programe for printing'
+ * Return : always 0 (Sucess)
+ */
 int main(void)
 {
-char Mystr[] = "\"Programming is like a multilingual puzzle";
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 puts(Mystr);
 
