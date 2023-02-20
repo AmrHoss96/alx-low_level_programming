5baf5689bb2021b2a82f671d3fcb10decf4840c4
Amr <amrhoss96@gmail.com>
2023-02-20 15:41:28 -0800

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 /**
   * main - Entry point
-  * 
+  *
   * Return: 0 Always (Sucess)
   */
 int main(void)
 {
 
-	int i,j,k;
+	int i, j, k;
 
 	i = 0;
 
 	while (i < 100)
+
 	{
 
 	 k = i / 10; /* first number */
-	
+
 	 j = i % 10; /* second number */
 
 		if (k < j)
 		{
 			putchar(k + '0');
 			putchar(j + '0');
-		
+
 		if (i < 89)
 		{
 			putchar(',');
