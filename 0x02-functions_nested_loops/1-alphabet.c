eefb4419642469515fd2a6c3e50231de3d297c3e
Amr <amrhoss96@gmail.com>
2023-02-21 15:42:06 -0800

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
   *
   * retun: 0 
   */
-int main(void)
+void print_alphabet(void)
 {
 	char i;
 	for (i = 'a'; i <= 'z'; i++)
@@ -12,5 +12,4 @@ int main(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
