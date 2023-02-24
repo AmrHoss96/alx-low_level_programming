8a00ea5d272d55133ccd6e664f8c5a9535ee4a5b
Amr <amrhoss96@gmail.com>
2023-02-23 17:05:54 -0800

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * isupper - check code
+ *
+ * return: 1 if true and 0 if false
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
