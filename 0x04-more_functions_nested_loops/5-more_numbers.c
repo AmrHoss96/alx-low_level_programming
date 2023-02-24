2ea55ce0c7410e5248364b7633db9552f60cabc4
Amr <amrhoss96@gmail.com>
2023-02-23 18:05:08 -0800

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,24 +6,20 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int x, y;
 
-	int j;
-
-	for (i = 0; i < 10; i++)
+	for (x = 0; x < 10; x++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (j <= 9)
+			if (y > 9)
 			{
-				_putchar(j + '0');
+				_putchar((y / 10) + '0');
 			}
-			else
-			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
+
+			_putchar((y % 10) + '0');
 		}
+
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
