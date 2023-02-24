3215128a8bd0e5d4b2e44dbcae2f856f0367e4ed
Amr <amrhoss96@gmail.com>
2023-02-23 18:14:29 -0800

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints line
+ * @n: number of _
+ * Retrun: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n == 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(n + '_');
+		}
+		_putchar('\n');
+	}
+}
