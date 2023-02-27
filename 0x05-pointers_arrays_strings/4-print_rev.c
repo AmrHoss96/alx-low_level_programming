527cfd2e85157f4e4598abca1857cae9a9ce1149
Amr <amrhoss96@gmail.com>
2023-02-27 15:09:17 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a line of text reversed
+ * @s: a string - array ofr characters
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (j = i - 1; s[j] != '\0'; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
