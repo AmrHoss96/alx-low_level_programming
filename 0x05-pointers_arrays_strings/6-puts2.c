0c817fa7488287c3033a02c06aa1c3c4117f459c
Amr <amrhoss96@gmail.com>
2023-02-27 16:22:27 -0800

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - prints even numbers of a string
+ * @str: array of numbers as characters
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
