49ffca4e394d86e83d3eef90ccc87a45af3ca815
Amr <amrhoss96@gmail.com>
2023-03-02 17:58:03 -0800

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,31 +5,38 @@
  * @str: string.
  * Return: string.
  */
-char *leet(char *str) {
-    static char result[256]; // buffer to hold the encoded string
-    int i, j;
-    char c;
+/**
+ * *leet - check the code for Holberton School students.
+ *
+ * @str: string.
+ * Return: string.
+ */
+char *leet(char *str)
+{
+	int i;
 
-    for (i = 0, j = 0; str[i] != '\0'; i++) {
-        c = str[i];
-
-        // check if c is one of the letters to be replaced
-        if (c == 'a' || c == 'A') {
-            result[j++] = '4';
-        } else if (c == 'e' || c == 'E') {
-            result[j++] = '3';
-        } else if (c == 'o' || c == 'O') {
-            result[j++] = '0';
-        } else if (c == 't' || c == 'T') {
-            result[j++] = '7';
-        } else if (c == 'l' || c == 'L') {
-            result[j++] = '1';
-        } else {
-            result[j++] = c;
-        }
-    }
-
-    result[j] = '\0'; // terminate the result string
-    return result;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		}
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+	}
+	return (str);
 }
-
