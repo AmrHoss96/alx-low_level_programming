94637d91a7018c2de1374020fb2ce7927b5ea032
AMR <amrhoss96@gmail.com>
2023-03-11 04:03:06 +0200

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vevtor
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
