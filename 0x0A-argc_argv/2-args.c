54e6bfc8a2a4cab181ee55f8b222dbd4c077e5b4
AMR <amrhoss96@gmail.com>
2023-03-11 03:58:08 +0200

    betty changes

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,13 +5,13 @@
  * @argv: argument vevtor
  * Return:0
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    int i;
+	int i;
 
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
-    return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
