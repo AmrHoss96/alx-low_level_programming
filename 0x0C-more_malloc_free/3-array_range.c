ee4dbe0094b49d008d5d62f2ccb89d6a37160933
AMR <amrhoss96@gmail.com>
2023-03-18 04:40:34 +0200

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,8 +19,7 @@ int *array_range(int min, int max)
 	if (ptr == NULL)
 		return (NULL);
 	ptr[0] = min;
-	for (i = 1; i <= max; i++)
+	for (i = min; i <= max; i++)
 		ptr[i] = i;
-	ptr[i] = max;
 	return (ptr);
 }
