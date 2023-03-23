1fbf3c67a1d55bdb9598d9539fbc6a9405299d5d
AMR <amrhoss96@gmail.com>
2023-03-24 01:17:00 +0200

    edit task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,49 @@
-include "main.h"
+#include "main.h"
 
-int actual_prime(int n, int i);
+/**
+ * is_prime_helper - says if an integer is a prime number or not
+ * @n: number to evaluate
+ * @factor: helper
+ * Return: 1 if n is a prime number, 0 if not
+ */
+int is_prime_helper(int n, int factor)
+{
+	if (n < 2)
+	{
+	/* Negative numbers, 0, and 1 are not prime */
+	return (0);
+	}
+	if (n == 2)
+	{
+	/* 2 is the only even prime number */
+	return (1);
+	}
+	if (n % 2 == 0)
+	{
+	/* Even numbers (except 2) are not prime */
+	return (0);
+	}
+	if (factor * factor > n)
+	{
+	/* n is not divisible by any factor up to its square root, so it's prime */
+	return (1);
+	}
+	if (n % factor == 0)
+	{
+	/* n is divisible by factor, so it's not prime */
+	return (0);
+	}
+	/* Check the next odd factor */
+	return (is_prime_helper(n, factor + 2));
+}
 
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
- *
  * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (actual_prime(n, n - 1));
+	return (is_prime_helper(n, 3));
 }
 
-/**
- * actual_prime - calculates if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
- *
- * Return: 1 if n is prime, 0 if not
- */
-int actual_prime(int n, int i)
-{
-	if (i == 1)
-		return (1);
-	if (n % i == 0 && i > 0)
-		return (0);
-	return (actual_prime(n, i - 1));
-}
