9ff8ec3b8e871430022b74170d411eec61eba3a1
AMR <amrhoss96@gmail.com>
2023-04-01 04:44:36 +0200

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node_end - prints number of nodes
+ * @head: is a pointer to a pointer that head points to
+ * @str: pointer to a  string
+ * Return: pointer to structure
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *new_string;
+	unsigned int length;
+
+	new_node = *head;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+	if (str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	length = strlen(str);
+	new_string = strdup(str);
+	if (new_string == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = new_string;
+	new_node->len = length;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
