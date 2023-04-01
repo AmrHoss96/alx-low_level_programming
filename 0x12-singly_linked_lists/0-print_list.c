a3e7efed28bcbc64e11e1f50a0c55e19ed39fdab
AMR <amrhoss96@gmail.com>
2023-04-01 02:39:56 +0200

    taske 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_list - prints number of nodes
+ * @h: struct pointer
+ * Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		if (current->str != NULL)
+		{
+			printf("[%d] %s\n", current->len, current->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
