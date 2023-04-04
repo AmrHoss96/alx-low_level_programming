f83cbfe4d36b4e3454df6303f75617db67f633de
AMR <amrhoss96@gmail.com>
2023-04-05 00:20:58 +0200

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - adds node to a linked list
+ * @head: pointer to a pointer of the first node
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+	listint_t *ptr;
+
+	current = *head;
+	while (current != NULL)
+	{
+		ptr = current->next;
+		free(current);
+		current = ptr;
+	}
+	*head = NULL;
+}
