b12c60ee8bbfb803f08e5c3d814c75c005c0ee4f
AMR <amrhoss96@gmail.com>
2023-04-05 21:14:54 +0200

    betty for task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,32 +7,32 @@
  * Return: 1 if error else return 0
 */
 int main(int argc, char *argv[])
-{       
-        int change, num;
+{
+	int change, num;
 
-        change = 0;
-        if (argc == 2)
-        {
-                num = atoi(argv[1]);
-                if (num > 0)
-                {
-                        for (; num > 25; change ++)
-                                num -= 25;
-                        for (; num > 10; change ++)
-                                num -= 10;
-                        for (; num > 5; change ++)
-                                num -= 5;
-                        for (; num > 2; change ++)
-                                num -= 2;
-                        for (; num > 1; change ++)
-                                num -= 1;       
-                }
-        printf("%d\n", change);
-        return (0);
-        }
-        else
-        {
-                printf("Error\n");
-                return (1);
-        }
+	change = 0;
+	if (argc == 2)
+	{
+		num = atoi(argv[1]);
+		if (num > 0)
+		{
+			for (; num > 25; change++)
+				num -= 25;
+			for (; num > 10; change++)
+				num -= 10;
+			for (; num > 5; change++)
+				num -= 5;
+			for (; num > 2; change++)
+				num -= 2;
+			for (; num > 1; change++)
+				num -= 1;
+		}
+	printf("%d\n", change);
+	return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
