195102364f028f81c0a816f73fe22a2887d56d10
AMR <amrhoss96@gmail.com>
2023-04-05 02:09:15 +0200

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_nodeint_at_index - get nodes at certain index
+ * @head: pointer to the first node
+ * @index: index of nodes
+ * Return:  pointer to the first node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr, *ptr2;
+	unsigned int i;
+
+	ptr = head;
+	ptr2 = malloc(sizeof(listint_t));
+	ptr2->next = NULL;
+	if (ptr2 == NULL)
+		return (NULL);
+	i = 0;
+	while (ptr != NULL)
+	{
+		if (i == index)
+			return (ptr);
+		ptr = ptr->next;
+		i++;
+	}
+	return (ptr);
+}
