01d8fc250387633a50d5b072425ee03852fdf470
AMR <amrhoss96@gmail.com>
2023-04-05 22:39:10 +0200

    task 100 first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void first(void)__attribute__ ((constructor));
+
+/**
+ * first - prints from main
+ * 
+*/
+void first(void)
+{
+	printf("Your're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my bach!\n");
+}
