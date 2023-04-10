a1b2ccff38c91a4f43e3b611c9ac995fb20df166
AMR <amrhoss96@gmail.com>
2023-04-10 02:13:10 +0200

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - prints number in binary
+ * @n: takes unsigned integer as an argument
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int step;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (temp = n, step = 0; (temp >>= 1) > 0; step++)
+		;
+	for (; step >= 0; step--)
+	{
+		if ((n >>step) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
